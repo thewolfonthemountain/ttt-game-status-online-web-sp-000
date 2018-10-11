f98f000665b4f85aa258633770f04709527880f6
Sean Langton <slangton1@gmail.com>
2018-10-11 00:49:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,7 +23,7 @@ def won?(board)
       board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
       return win_combination
     else
-      false
+      return false
     end
   end
 end

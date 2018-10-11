49cc539765cf6d5c3d9f0612c12fdd20ffb08269
Sean Langton <slangton1@gmail.com>
2018-10-11 00:45:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,8 @@ def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]]
       return win_combination
+    else
+      falsey
     end
-  end 
+  end
 end
